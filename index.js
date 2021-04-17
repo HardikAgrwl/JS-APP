d9e82c938cbbcba36687025c52728692ab4e4a9e
HardikAgrwl <hardikagarwal213227@example.com>
2021-04-17 15:48:30 +0530

    input output processing

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var btnTranslate = document.querySelector('#btn-translate');
 var textarea = document.querySelector('#text-area');
+var outputText = document.querySelector('#output')
 
 btnTranslate.addEventListener('click' , function clickEventHandler(){
-    console.log("input is : " , textarea.value);
+    outputText.innerText = textarea.value;
 })
