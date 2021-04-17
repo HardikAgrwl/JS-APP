350e1a613b59e12c1830621fc99aa042131dda35
HardikAgrwl <hardikagarwal213227@example.com>
2021-04-17 22:31:10 +0530

    Designing and functioning included

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-var btnTranslate = document.querySelector('#btn-translate');
-var textarea = document.querySelector('#text-area');
-var outputText = document.querySelector('#output')
+var btnTranslate = document.querySelector('#translate-button');
+var textInput = document.querySelector('#translate-input');
+var outputText = document.querySelector('#output');
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
-btnTranslate.addEventListener('click' , function clickEventHandler(){
-    outputText.innerText = textarea.value;
-})
+function getTransaltionUrl(text){
+    return serverURL + "?text=" + text;
+}
+function errorHandler(error){
+    alert('error occured');
+    console.log('error occured' , error);
+}
+
+function clickEventHandler(){
+    var inputText = textInput.value;
+
+    fetch(getTransaltionUrl(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translated = json.contents.translated;
+        outputText.innerText = translated;
+    })
+    .catch(errorHandler)
+}
+
+btnTranslate.addEventListener('click' , clickEventHandler)
